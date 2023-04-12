823a809827779e2e19399d0155b4bb25734a39c2
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-04-12 19:20:56 +0100

    Test output

--- tests/test_eflomal.py
+++ tests/test_eflomal.py
@@ -14,15 +14,27 @@ DATADIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'testda
 class TestAlign(unittest.TestCase):
 
     def setUp(self):
-        with open(os.path.join(DATADIR, 'test1.sv'), 'r', encoding='utf-8') as fobj:
-            self.src_data = fobj.readlines()
-        with open(os.path.join(DATADIR, 'test1.en'), 'r', encoding='utf-8') as fobj:
-            self.trg_data = fobj.readlines()
-        with open(os.path.join(DATADIR, 'test1.priors'), 'r', encoding='utf-8') as fobj:
-            self.priors_data = fobj.readlines()
+        self.src_data = [
+            'En svart katt .\n',
+            'En gul fågel .\n',
+            'En vit elefant .\n',
+        ]
+        self.trg_data = [
+            'A black cat .\n',
+            'A yellow bird .\n',
+            'A white elephant .\n',
+        ]
+        self.priors_data = [
+            'LEX\tsvart\tblack\t100\n',
+            'LEX\tkatt\tcat\t100\n',
+            'LEX\tgul\tyellow\t100\n',
+            'LEX\tfågel\tbird\t100\n',
+            'LEX\tvit\twhite\t100\n',
+            'LEX\telefant\telephant\t100\n',
+        ]
 
     def test_aligner(self):
-        """Test aligner"""
+        """Test aligner without priors producing rubbish"""
         aligner = eflomal.Aligner()
         with tempfile.NamedTemporaryFile('w+') as fwd_links, \
              tempfile.NamedTemporaryFile('w+') as rev_links:
@@ -30,9 +42,18 @@ class TestAlign(unittest.TestCase):
                           links_filename_fwd=fwd_links.name, links_filename_rev=rev_links.name,
                           quiet=False)
             fwd_links.seek(0)
-            self.assertEqual(len(fwd_links.readlines()), 3)
+            self.assertEqual(fwd_links.readlines(), [
+                '3-1 1-2 3-3\n',
+                '2-1 1-2 3-3\n',
+                '2-1 1-2 3-3\n'
+            ])
+            
             rev_links.seek(0)
-            self.assertEqual(len(rev_links.readlines()), 3)
+            self.assertEqual(rev_links.readlines(), [
+                '1-3 2-1 3-3\n',
+                '1-2 2-1 3-3\n',
+                '1-2 2-1 3-3\n'
+            ])
 
     def test_aligner_with_priors(self):
         """Test aligner with priors"""
@@ -43,9 +64,18 @@ class TestAlign(unittest.TestCase):
                           links_filename_fwd=fwd_links.name, links_filename_rev=rev_links.name,
                           priors_input=self.priors_data, quiet=False)
             fwd_links.seek(0)
-            self.assertEqual(len(fwd_links.readlines()), 3)
+            self.assertEqual(fwd_links.readlines(), [
+                '0-0 1-1 2-2 3-3\n',
+                '0-0 1-1 2-2 3-3\n',
+                '0-0 1-1 2-2 3-3\n'
+            ])
+            
             rev_links.seek(0)
-            self.assertEqual(len(rev_links.readlines()), 3)
+            self.assertEqual(rev_links.readlines(), [
+                '0-0 1-1 2-2 3-3\n',
+                '0-0 1-1 2-2 3-3\n',
+                '0-0 1-1 2-2 3-3\n'
+            ])
 
     def test_makepriors(self):
         """Test creating priors"""
